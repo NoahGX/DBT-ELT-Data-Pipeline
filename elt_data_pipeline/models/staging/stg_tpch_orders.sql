955b7f44a887092d20f1ef1993710b3598f5b048
NoahGX <noahjgarcia2019@gmail.com>
2024-10-01 22:53:22 -0700

    Update

--- elt_data_pipeline/models/staging/stg_tpch_orders.sql
+++ elt_data_pipeline/models/staging/stg_tpch_orders.sql
@@ -1,7 +1,2 @@
-select
-    o_orderkey as order_key,
-    o_custkey as customer_key,
-    o_orderstatus as status_code,
-    o_totalprice as total_price,
-    o_orderdate as order_date
+select *
 from {{ source('tpch', 'orders') }}
