0d7d8dec5f5df905f57dd15d06a4c36f0f42f6a9
NoahGX <noahjgarcia2019@gmail.com>
2024-10-08 00:25:07 -0700

    Update

--- dbt-dag/dags/dbt_dag.py
+++ dbt-dag/dags/dbt_dag.py
@@ -10,14 +10,14 @@ profile_config = ProfileConfig(
     profile_mapping=SnowflakeUserPasswordProfileMapping(
         conn_id="snowflake_conn",
         profile_args={"database": "dbt_db", "schema": "dbt_schema"},
-    )
+    ),
 )
 
-dbt_snowflake_dag= DbtDag(
-    profile_config=ProjectConfig("xxx",),
+dbt_snowflake_dag = DbtDag(
+    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/elt_data_pipeline",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
-    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ[xxx],}"),
+    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
     schedule_interval="@daily",
     start_date=datetime(2024, 10, 8),
     catchup=False,
